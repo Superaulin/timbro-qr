dc16cf131708dede97fd7a4036aa3f23d0dc5780
Superaulin <simonecasti@gmail.com>
2025-12-08 22:06:54 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js – configurazione centralizzata per tutto il progetto TimbroQR
+
+window.TIMBRO_CONFIG = {
+    // URL Web App Apps Script (unico posto da cambiare se Google rompe le palle)
+    apiBaseUrl: "https://script.google.com/macros/s/AKfycbw3UYl68d61ym-mPVrgh1QyphQaaWotOSgrczzNGRS3KD9euIpDg-nv60Ns06vRCfLx/exec",
+
+    // in futuro puoi aggiungere altro:
+    // tenantName: "RAM Energy E&C",
+    // theme: "dark",
+};
